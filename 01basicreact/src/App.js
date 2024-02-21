3061395c49fae515ca2df9d396335a8963113fd0
mahiboobnadaf <rasool97260@gmail.com>
2024-02-21 18:38:50 +0530

    Button is added

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,9 +1,18 @@
  
+
+function MyButton(){
+  return (
+    <button>I am Button</button>
+  )
+} 
+
 function App() {
   return (
     <>
       <h1>Hello i am Mahiboob</h1>
       <h2>Welcom to React</h2>
+      <MyButton />
+
     </>
   );
 }
