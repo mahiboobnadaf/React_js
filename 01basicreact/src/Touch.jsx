4ebf631f9a135b13e5c8d085bada8c6742ce2ebf
mahiboobnadaf <rasool97260@gmail.com>
2024-02-23 20:38:17 +0530

    Aded explations

--- 01basicreact/src/Touch.jsx
+++ 01basicreact/src/Touch.jsx
@@ -1,3 +1,13 @@
+//There are rules for Writing components
+// 1. Component first letter should be capital
+// 2. only single tag can written (wrapped) 
+// for multiple tags  use 
+// <> 
+// <h1> <h2> <h3> .... etc
+ // </>
+ // <> Wrap your tags in this fragments </> 
+
+
 function Touch(){
     return (
         <h3> Touch Me if you can</h3>
