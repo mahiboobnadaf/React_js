488158c9683922df30a09c62e2f506b7baf7f791
mahiboobnadaf <rasool97260@gmail.com>
2024-02-24 16:14:51 +0530

    React CreateElement method explained with example

--- viteReact/src/main.jsx
+++ viteReact/src/main.jsx
@@ -11,10 +11,21 @@ function MyApp(){
 
 const anotherElement = (<a href='https://google.com' target='_blank'>Visit for Google</a>);
 
+const reactElement = React.createElement(
+      'a',
+      {href:'https://google.com',target:'_blank'},
+      "Click me Click me Now"
+      )
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   
     // MyApp()          //We can also call it like this,if there is no fragment.In
                         // render we can acces only one member if ther is no fragments
-    anotherElement      //Then we can also access a function expression also it works only if no fragments are used 
-  
+    // anotherElement      //Then we can also access a function expression also it works only if no fragments are used 
+    
+    reactElement      // Via this method we can access directly through objects provided by React. 
+                      // the same this happens when we create tags in components , tags will be converted into TreeGraph structure
+
 )
+
+
