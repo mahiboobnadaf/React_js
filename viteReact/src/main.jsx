8fa675244be67b5e00999f9d19748af46fcba46a
mahiboobnadaf <rasool97260@gmail.com>
2024-02-24 15:57:10 +0530

    Without fragments, using a function same as in js

--- viteReact/src/main.jsx
+++ viteReact/src/main.jsx
@@ -9,9 +9,12 @@ function MyApp(){
   )
 }
 
+const anotherElement = (<a href='https://google.com' target='_blank'>Visit for Google</a>);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <> <App />
-    <MyApp /> 
-     </>
+  
+    // MyApp()          //We can also call it like this,if there is no fragment.In
+                        // render we can acces only one member if ther is no fragments
+    anotherElement      //Then we can also access a function expression also it works only if no fragments are used 
   
 )
