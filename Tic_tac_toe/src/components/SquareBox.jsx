0c9d9e0773156842e3c5f997973ebec18f3a8178
mahiboobnadaf <rasool97260@gmail.com>
2024-03-11 16:00:56 +0530

    asvd

--- Tic_tac_toe/src/components/SquareBox.jsx
+++ Tic_tac_toe/src/components/SquareBox.jsx
@@ -2,13 +2,25 @@ import "../App.css"
 import { useState } from "react"
 
     function Square(){
-        let [value,setValue] = useState(0)
+        let [value,setValue] = useState(null)
+        let [turn,setTurn] = useState(null)
+
         
         function handleClick(){
-            setValue(value+1)
+            if(value=='X'){
+                setValue('0')
+            }
+            else if(value=='O'){
+                setValue('X')
+            }
+            else{
+                setValue(value='X')
+            }
+            console.log(value)
            
-            
-        }
+        }  
+             
+        
         
         return (
             <button className='Boxheight' onClick={handleClick}>{value}</button>
