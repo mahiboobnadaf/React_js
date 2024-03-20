0bcbbf0e3c69fbbe3060bb3abbd9057789da4f07
mahiboobnadaf <rasool97260@gmail.com>
2024-03-20 15:39:21 +0530

    Changed Name of person

--- Tailwind_03/src/comp1.jsx
+++ Tailwind_03/src/comp1.jsx
@@ -6,7 +6,7 @@ function Compo(){
               <blockquote>
                 <p className="text-lg">
                   “Tailwind CSS is the only framework that I've seen scale
-                  on large teams. It’s easy to customize, adapts to any design,
+                  on large teams. It's easy to customize, adapts to any design,
                   and the build size is tiny.”
                 </p>
               </blockquote>
