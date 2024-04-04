6c6d49cd905edaa39f9ea90fc05c7b1a1689fae7
mahiboobnadaf <rasool97260@gmail.com>
2024-04-04 14:37:44 +0530

    Tailwind configured and tested working

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 
 
 function App() {
-  const [color, setColor] = useState("olive")
+  const [color, setColor] = useState('olive')
 
   return (
-    <>
-      <div>
-
+    
+      <div className='h-screen w-full border-2 border-black' style={{backgroundColor: color}}> 
+        <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 rounded border-2 border-black'> Rasool </div>
       </div>
-    </>
+    
+    
+   
   )
 }
 
