d952725912bd2ff1561a02bc2a880a819e0aaade
mahiboobnadaf <rasool97260@gmail.com>
2024-04-04 16:10:57 +0530

    extra code removed

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -3,13 +3,11 @@ import { useState } from 'react'
 
 function App() {
   const [color, setColor] = useState('olive')
-  let whiteColor='flex flex-wrap justify-center gap-3 rounded-xl px-2 py-2 border-2 border-black bg-white'
  
-
   return (
     
-      <div className='h-screen w-full border-2 border-black flex justify-center items-center font-semibold text-xl' style={{backgroundColor: color}}> 
-        Mahiboob Nadaf
+      <div className='h-screen w-full border-2 border-black flex justify-center items-center font-semibold text-lg' style={{backgroundColor: color}}> 
+        <p className='text-[48px]'>Mahiboob Nadaf</p>
           <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 rounded  px-2'> 
               <div className='flex flex-wrap justify-center gap-3 rounded-3xl px-2 py-2 border-2 border-black bg-white items-center font-serif font-bold'> 
                 Select Color to Change :
@@ -19,10 +17,6 @@ function App() {
                 <button onClick={()=>setColor("gray")} className='px-4 py-1 rounded-full  shadow-lg' style={{backgroundColor:"gray"}}>Gray</button>
               
               </div>
-              {/* <div className={redColor}>red</div>
-              <div className={greenColor}>green</div>
-              <div className={yellowColor}>yellow</div>
-              <div className={silverColor}>silver</div> */}
           </div>
       </div>
     
